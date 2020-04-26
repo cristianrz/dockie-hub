c917325535bb0be54668e6928c3d9f5ab93d9b07
cristianrz <dev@cristianrz.com>
2020-04-26 23:05:01 +0100

    Fix bootstrap for crux

--- library/crux/bootstrap.sh
+++ library/crux/bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 set -eu
 NAME="crux"
 RELEASE="3.5"
@@ -7,12 +8,13 @@ FILENAME="$NAME-$RELEASE.iso"
 ORIG="$HOME/.local/lib/pocker/images/$NAME"
 
 mkdir iso
-mv "$FILENAME" iso
+cp "$ORIG/$FILENAME" iso
 cd iso
 7z x "$FILENAME"
 mv rootfs.tar.xz ../
 cd ..
 rm -rf iso
-tar xf "$FILENAME"
-rm -rf "$FILENAME"
+pwd
+tar xf rootfs.tar.xz
+#rm -rf rootfs.tar.xz
 
